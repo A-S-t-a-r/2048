295a587dbcc4e55de141c0c7aec9cd5abfa76e68
Chen Xing <2025212633@mail.hfut.edu.cn>
2025-12-14 14:58:16 +0800

    Initialize
    Initialize the project structure.

---
+++ Game2048/src/main.cpp
@@ -0,0 +1,22 @@
+﻿#include"Base.h"
+
+int main() {
+    // 创建窗口
+    initgraph(MAX_GRID * GRID_WIDTH + 5 * INTERVAL,
+        MAX_GRID * GRID_WIDTH + 5 * INTERVAL);
+
+    // 创建Game对象
+    Game* game = new Game();
+
+    while (true) {
+        game->Draw(); 
+        game->Control();
+        game->Update();
+    }
+
+    delete game;
+
+    getchar();
+
+    return 0;
+}

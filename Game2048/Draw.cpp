f458731c42e7ec3c118640629fef4ac91328e0ed
Chen Xing <2025212633@mail.hfut.edu.cn>
2025-12-14 16:53:52 +0800

    Feat
    Add Draw class.

--- Game2048/Draw.cpp
+++ Game2048/Draw.cpp
@@ -1,1 +1,27 @@
 #include "Draw.h"
+
+void Draw::Render() {
+    setbkcolor(back);
+    cleardevice();
+    for (int i = 0; i < MAX_GRID; i++) {
+        for (int j = 0; j < MAX_GRID; j++) {
+            for (int k = 0; k < 12; k++) {
+                if (map[i][j] == num[k]) {
+                    setfillcolor(arr[k]);
+                    solidrectangle(pos[i][j].x, pos[i][j].y,
+                        pos[i][j].x + GRID_WIDTH, pos[i][j].y + GRID_WIDTH);
+                    if (map[i][j] != 0) {
+                        char number[5] = " ";
+                        settextcolor(RGB(119, 110, 101));
+                        settextstyle(50, 0, _T("楷体"));  // 使用正确的字体名称
+                        setbkmode(TRANSPARENT);
+                        sprintf_s(number, "%d", map[i][j]);
+                        int tempx = GRID_WIDTH / 2 - textwidth(number) / 2;
+                        int tempy = GRID_WIDTH / 2 - textheight(number) / 2;
+                        outtextxy(pos[i][j].x + tempx, pos[i][j].y + tempy, number);
+                    }
+                }
+            }
+        }
+    }
+}
--- Game2048/Draw.h
+++ Game2048/Draw.h
@@ -1,1 +1,9 @@
 #pragma once
+#include "GridGame.h"
+
+class Draw : virtual public GridGame {
+public:
+    Draw() {}
+    virtual ~Draw() {}
+    void Render();
+};
